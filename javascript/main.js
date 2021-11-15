0c85071ef1c27ce64f824d32993036246cbc649a
Ben <66697315+Boxit379@users.noreply.github.com>
2021-11-14 21:33:20 -0700

    Making progress (bars)

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,23 @@
 var trashRemoved = 0;
+var totalTrashRemoved = 0;
+var trashTimer = -1;
+var timerMax = 500;
 
-function removeTrash(trashAmount){
-    trashRemoved = trashRemoved + trashAmount;
-    document.getElementById("trashCounter").innerHTML = trashRemoved; 
-};
+function removeTrash(){
+  if (trashTimer == -1) {
+    trashTimer = 0;
+  }
+}
+
+window.setInterval(function(){
+  if (trashTimer < timerMax && trashTimer >= 0) {
+    trashTimer += 1;
+  }
+  else if (trashTimer >= timerMax) {
+    trashTimer = -1;
+    trashRemoved += 1;
+    totalTrashRemoved += 1;
+  }
+  document.getElementById("trashCounter").innerHTML = trashRemoved;
+  document.getElementById("trashProgress").value = trashTimer;
+}, 10);
