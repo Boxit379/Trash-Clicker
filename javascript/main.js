c866acc8ab0e41c66df79396f1b0db2c2e8adaa8
Ben <66697315+Boxit379@users.noreply.github.com>
2021-11-16 20:51:28 -0700

    upgrades and stuff

--- javascript/main.js
+++ javascript/main.js
@@ -3,9 +3,34 @@ var totalTrashRemoved = 0;
 var trashTimer = -1;
 var timerMax = 500;
 
+var savegame = JSON.parse(localStorage.getItem("save"));
+if (savegame != null) {
+  if (typeof savegame.trashRemoved !== "undefined") trashRemoved = savegame.trashRemoved;
+  if (typeof savegame.totalTrashRemoved !== "undefined") totalTrashRemoved = savegame.totalTrashRemoved;
+  if (typeof savegame.timerMax !== "undefined") timerMax = savegame.timerMax;
+}
+
+$("#upgradesTab").hide();
+hideCheck();
+
 function removeTrash(){
   if (trashTimer == -1) {
     trashTimer = 0;
+  }
+}
+
+function saveGame() {
+  var save = {
+    trashRemoved: trashRemoved,
+    totalTrashRemoved: totalTrashRemoved,
+    timerMax: timerMax
+  }
+  localStorage.setItem("save",JSON.stringify(save));
+}
+
+function hideCheck() {
+  if (totalTrashRemoved >= 3) {
+    $("#upgradesTab").show();
   }
 }
 
@@ -20,4 +45,5 @@ window.setInterval(function(){
   }
   document.getElementById("trashCounter").innerHTML = trashRemoved;
   document.getElementById("trashProgress").value = trashTimer;
+  hideCheck();
 }, 10);
